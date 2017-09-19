3e32732aae6aae3a254bad18941bcd3d4db11fa5
JJ <infernoj2581@gmail.com>
2017-09-19 09:33:36 -0600

    Added a display class

---
+++ src/mosnter/view/monsterDisplay.java
@@ -0,0 +1,10 @@
+package mosnter.view;
+
+import javax.swing.JOptionPane;
+
+
+public class monsterDisplay {
+	public void displayText(String textToDisplay) {
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+}
