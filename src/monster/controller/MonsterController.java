25c86244aa92464954b8efa9d80f3002d59fc61e
JJ <infernoj2581@gmail.com>
2017-09-21 09:04:35 -0600

    Yo Mang, I done did stuff with popupdisplay and catching stuff mang

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -10,43 +10,65 @@ public class MonsterController {
 	public void start() {
 		MarshmallowMonster sample = new MarshmallowMonster("World Ender",2,4,3,true);
 		System.out.println(sample);
-		System.out.println("He ate his own bloops!" + "\n");
+//		System.out.println("He ate his own bloops!" + "\n");
+		popup.displayText("He ate his own bloops!");
 		sample.setHasBloop(false);
-		System.out.println(sample);
+//		System.out.println(sample);
+		popup.displayText(sample.toString());
 		scanner(sample);
 	}
 	public void scanner(MarshmallowMonster currentMonster) {
 		Scanner in = new Scanner(System.in);
-		int inpNum = 0;
 		int con = 0;
+		int inpNum = 5;
 		while(con == 0) {
 			System.out.print("What do you want to eat: ");
-			String inp = in.nextLine();
+// 	  	    String inp = in.nextLine();
+			String inp = popup.getResponse("What do you want to eat: ");
 			if (inp.contains("eye")) {
-				System.out.println("\n" + "I have " + currentMonster.getEyeCount() + " " + inp);
-				System.out.print("\n" + "How much of " + inp + " do you want to eat: ");
-				inpNum = in.nextInt();	
+//				System.out.println("\n" + "I have " + currentMonster.getEyeCount() + " " + inp);
+				popup.displayText("I have " + currentMonster.getEyeCount() + " eyes");
+//				System.out.print("\n" + "How much of " + inp + " do you want to eat: ");
+				String inpNumYe = popup.getResponse("How much do you want to it: ");
+				try {
+				 	inpNum = Integer.parseInt(inpNumYe);
+				}
+				catch(Exception e) {
+					System.out.println("Whatcu want kid");
+					popup.displayText("Watchu want kid");
+				}
 				int negEye = currentMonster.getEyeCount() - inpNum;
 				if (negEye >= 0) {
-					System.out.println(currentMonster.getName() + " ate his own " + inp + " He now have " + negEye + " eyes." + "\n");
+//					System.out.println(currentMonster.getName() + " ate his own " + inp + " He now have " + negEye + " eyes." + "\n");
+					popup.displayText(currentMonster.getName() + " ate his own " + inp + " He now have " + negEye + " eyes.");
 					currentMonster.setEyeCount(negEye);
 				}
 				else {
-					System.out.println("You can't do that!" + "\n");
+//					System.out.println("You can't do that!" + "\n");
+					popup.displayText("You can't do that!");
 				}
 			}
 			
 			else if (inp.contains("arm")) {
-				System.out.println("\n" + "I have " + currentMonster.getArmCount() + " " +inp);
-				System.out.print("\n" + "How much of " + inp + " do you want to eat: ");
-				inpNum = in.nextInt();
+//				System.out.println("\n" + "I have " + currentMonster.getArmCount() + " " +inp);
+				popup.displayText("I have " + currentMonster.getArmCount() + " " +inp);
+//				System.out.print("\n" + "How much of " + inp + " do you want to eat: ");				
+				String numYeTwo = popup.getResponse("How much of " + inp + " do you want to eat: ");
+				try {
+					inpNum = Integer.parseInt(numYeTwo);
+				}
+				catch(Exception E) {
+					System.out.println("Really kid?");
+				}
 				int negArm = currentMonster.getArmCount() - inpNum;
 				if (negArm >= 0 ) {
-					System.out.println(currentMonster.getName() + " ate his own " + inp + " He now have " + negArm + " arms" + "\n");
+					popup.displayText(currentMonster.getName() + " ate his own " + inp + " He now have " + negArm + " arms");
+//					System.out.println(currentMonster.getName() + " ate his own " + inp + " He now have " + negArm + " arms" + "\n");
 					currentMonster.setArmCount(negArm);
 				}
 				else {
-					System.out.println("You can't do that!" + "\n");
+					popup.displayText("You can't do that!");
+//					System.out.println("You can't do that!" + "\n");
 				}
 			}
 			
@@ -57,6 +79,7 @@ public class MonsterController {
 				if (negTent >= 0) {
 					System.out.println(currentMonster.getName() + " ate his own " + inp + " He now have " + negTent + " tentacles" + "\n");
 					currentMonster.setTentacleAmount(negTent);
+					con++;
 				}
 				else {
 					System.out.println("You can't do that!");
@@ -64,10 +87,11 @@ public class MonsterController {
 			}
 			else {
 				System.out.println("What the heck");
+				popup.displayText("Yo mang, stop this nonsense");
 			}
-			popup.displayText("How ya doing");
-			String answer = popup.getResponse("Watchu eating for lunch?");
-			popup.displayText(answer);
 		}
+		popup.displayText("How ya doing");
+		String answer = popup.getResponse("Watchu eating for lunch?");
+		popup.displayText("You typed in " + answer);
 	}	
 }
--- src/mosnter/view/monsterDisplay.java
+++ src/mosnter/view/monsterDisplay.java
@@ -7,9 +7,9 @@ public class monsterDisplay {
 	public void displayText(String textToDisplay) {
 		JOptionPane.showMessageDialog(null, textToDisplay);
 	}
-	public String getResponse(String textToDisplay) {
+	public String getResponse(String questionAsked) {
 		String response = "";
-		response += JOptionPane.showInputDialog(null,textToDisplay);
+		response += JOptionPane.showInputDialog(null,questionAsked);
 		return response;
 	}
 }
